c730541e528765e7975cdd4fb1cae8cd9ad52842
narspi <ivanovmichurina@gmail.com>
2023-09-22 14:03:17 +0300

    socket.io add

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const express = require('express');
-const WebSocket = require('ws');
-const cors = require('cors'); 
-
-
 const app = express();
-app.use(cors());
+const cors = require('cors');
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server,{
+  cors: {
+    origin: '*'
+  }
+});
 
-// Создаем веб-сервер с помощью Express
-const server = app.listen(3000, () => {
-    console.log('Сервер запущен на порту 3000');
+const clients = {};
+const messages = [];
+
+io.on('connection', (socket) => {
+  console.log('Новое подключение Socket.io');
+
+  // Отправляем текущие сообщения клиенту при подключении
+  socket.emit('initialMessages', messages);
+
+  socket.on('sendMessage', (message) => {
+    messages.push(message);
+    io.emit('newMessage', message); // Рассылка нового сообщения всем клиентам
   });
-  
-  // Создаем WebSocket-сервер, привязанный к веб-серверу
-  const wss = new WebSocket.Server({ server });
-  
-  // Обработчик для новых подключений WebSocket
-  wss.on('connection', (ws) => {
-    console.log('Новое подключение WebSocket');
-  
-    // Обработчик для получения сообщений от клиента
-    ws.on('message', (message) => {
-      console.log(`Получено сообщение: ${message}`);
-  
-      // Отправляем обратно клиенту тоже самое сообщение
-      ws.send(`Yjd: ${message}`);
-    });
-  
-    // Обработчик закрытия соединения клиентом
-    ws.on('close', () => {
-      console.log('WebSocket соединение закрыто');
-    });
-  });
-  
 
+  socket.on('disconnect', () => {
+    console.log('Socket.io соединение закрыто');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Сервер запущен на порту 3000');
+});
